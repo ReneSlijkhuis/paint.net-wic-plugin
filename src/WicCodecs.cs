13d24244b34a35c2d11fb08b7d212bd2d37baafd
Rene Slijkhuis <rene@slijkhuis.org>
2017-08-15 18:49:40 +0200

    Added the source code

---
+++ src/WicCodecs.cs
@@ -0,0 +1,96 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//    MIT License
+//
+//    Copyright(c) 2017 René Slijkhuis
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Win32;
+
+namespace PaintDotNet.WicDecoder
+{
+    public static class WicCodecs
+    {
+        public static string[] GetSupportedExtensions()
+        {
+            Dictionary<string, string> extensionDictionary = new Dictionary<string, string>();
+
+            using (var regKeyClassesRoot = RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, RegistryView.Registry64))
+            using (var regKeyCodecs = regKeyClassesRoot.OpenSubKey(@"CLSID\{7ED96837-96F0-4812-B211-F13C24117ED3}\Instance", false))
+            {
+                if (regKeyCodecs != null)
+                {
+                    var subkeys = regKeyCodecs.GetSubKeyNames();
+
+                    foreach (string subkey in subkeys)
+                    {
+                        using (var regKeyCodec = regKeyClassesRoot.OpenSubKey(@"CLSID\{7ED96837-96F0-4812-B211-F13C24117ED3}\Instance\" + subkey, false))
+                        {
+                            if (regKeyCodec != null)
+                            {
+                                var clsid = (string)regKeyCodec.GetValue("CLSID", "");
+                                var extensionList = GetSupportedExtensionsFromCodec(clsid);
+
+                                if (extensionList != null)
+                                {
+                                    foreach (string extension in extensionList)
+                                    {
+                                        if (!extensionDictionary.ContainsKey(extension))
+                                        {
+                                            extensionDictionary.Add(extension, extension);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!extensionDictionary.ContainsKey(".bmp")) extensionDictionary.Add(".bmp", ".bmp");
+            if (!extensionDictionary.ContainsKey(".dib")) extensionDictionary.Add(".dib", ".dib");
+            if (!extensionDictionary.ContainsKey(".jpg")) extensionDictionary.Add(".jpg", ".jpg");
+            if (!extensionDictionary.ContainsKey(".jpeg")) extensionDictionary.Add(".jpeg", ".jpeg");
+            if (!extensionDictionary.ContainsKey(".gif")) extensionDictionary.Add(".gif", ".gif");
+            if (!extensionDictionary.ContainsKey(".tif")) extensionDictionary.Add(".tif", ".tif");
+            if (!extensionDictionary.ContainsKey(".tiff")) extensionDictionary.Add(".tiff", ".tiff");
+            if (!extensionDictionary.ContainsKey(".png")) extensionDictionary.Add(".png", ".png");
+
+            var finalList = new List<string>(extensionDictionary.Values);
+            finalList.Sort();
+            return finalList.ToArray();
+        }
+
+        private static List<string> GetSupportedExtensionsFromCodec(string clsid)
+        {
+            using (var regKeyClassesRoot = RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, RegistryView.Registry64))
+            using (var regKeyCodec = regKeyClassesRoot.OpenSubKey(@"CLSID\" + clsid, false))
+            {
+                if (regKeyCodec == null) return null;
+                var extensions = (string)regKeyCodec.GetValue("FileExtensions", "");
+                return extensions.ToLower().Split(',').ToList();
+            }
+        }
+    }
+}
---
+++ src/WicDecoder.cs
@@ -0,0 +1,135 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//    MIT License
+//
+//    Copyright(c) 2017 René Slijkhuis
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace PaintDotNet.WicDecoder
+{
+    public sealed class WicDecoderTypeFactory : IFileTypeFactory
+    {
+        public FileType[] GetFileTypeInstances()
+        {
+            return new[] { new WicDecoderType() };
+        }
+    }
+
+    internal class WicDecoderType : FileType
+    {
+        internal WicDecoderType() :
+            base("WIC Decoder", FileTypeFlags.SupportsLoading, WicCodecs.GetSupportedExtensions())
+        {
+        }
+
+        protected override Document OnLoad(Stream input)
+        {
+            FieldInfo fi = input.GetType().GetField("stream", BindingFlags.NonPublic | BindingFlags.Instance);
+            FileStream fs = fi.GetValue(input) as FileStream;
+
+            using (var image = LoadRAW(fs.Name))
+            {
+                var document = Document.FromImage(image);
+                var numberOfFrames = GetNumberOfFrames(fs.Name);
+                var size = new Size(image.Width, image.Height);
+
+                if (numberOfFrames <= 1)
+                {
+                    return document;
+                }
+                else
+                {
+                    document.Layers.Clear();
+                    for (int i = 0; i < numberOfFrames; i++)
+                    {
+                        using (var layer = LoadRAW(fs.Name, i))
+                        using (var surface = Surface.CopyFromBitmap(layer))
+                        {
+                            if (surface.Size == size)
+                            {
+                                var bitmapLayer = new BitmapLayer(surface);
+                                bitmapLayer.Name = "Frame " + (i + 1);
+                                document.Layers.Insert(0, bitmapLayer);
+                            }
+                        }
+                    }
+                    return document;
+                }
+            }
+        }
+
+        private static Bitmap LoadRAW(string filename, int frameIndex = 0)
+        {
+            var bitmapSource = LoadImage(filename, frameIndex);
+            return BitmapFromSource(bitmapSource);
+        }
+
+        private static BitmapSource LoadImage(string filename, int frameIndex)
+        {
+            using (var inFile = File.OpenRead(filename))
+            {
+                var decoder = BitmapDecoder.Create(inFile, BitmapCreateOptions.None, BitmapCacheOption.None);
+                return Convert(decoder.Frames[frameIndex]);
+            }
+        }
+
+        private static int GetNumberOfFrames(string filename)
+        {
+            using (var inFile = File.OpenRead(filename))
+            {
+                var decoder = BitmapDecoder.Create(inFile, BitmapCreateOptions.None, BitmapCacheOption.None);
+                return decoder.Frames.Count;
+            }
+        }
+
+        private static BitmapSource Convert(BitmapFrame frame)
+        {
+            int stride = frame.PixelWidth * (frame.Format.BitsPerPixel / 8);
+            byte[] pixels = new byte[frame.PixelHeight * stride];
+
+            frame.CopyPixels(pixels, stride, 0);
+
+            var bmpSource = BitmapSource.Create(frame.PixelWidth, frame.PixelHeight,
+                frame.DpiX, frame.DpiY, frame.Format, frame.Palette, pixels, stride);
+
+            return bmpSource;
+        }
+
+        public static Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            Bitmap bitmap;
+            using (var outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+            return bitmap;
+        }
+    }
+}
